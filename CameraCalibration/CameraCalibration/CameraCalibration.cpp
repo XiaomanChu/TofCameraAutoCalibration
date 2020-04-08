576d0baa7e31555a6e5fd475744210c2e98bce39
shiqian <379669328@qq.com>
2020-04-08 10:43:39 +0800

    Add: 畸变矫正预览

--- CameraCalibration/CameraCalibration/CameraCalibration.cpp
+++ CameraCalibration/CameraCalibration/CameraCalibration.cpp
@@ -1,22 +1,31 @@
 ﻿#include "CameraCalibration.h"
 using namespace std;
+using namespace cv;
 #pragma execution_character_set("utf-8")
+
 CameraCalibration::CameraCalibration(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
-
 	cornerOfRow = ui.cornersOfRowEdit->text().toInt();
-	cornerOfCol = ui.cornersOfColEdit->text().toInt();;
-
-	//部分槽连接
+	cornerOfCol = ui.cornersOfColEdit->text().toInt();
+	ui.currentParamShow->setText("当前畸变参数:\n CX =" + QString::number(C_CX) + "\n CY = " + QString::number(C_CY) + "\n FX = " + QString::number(C_FX) + "\n FY = " + QString::number(C_FY) + "\n K1 = " + QString::number(C_K1) + "\n K2 = " + QString::number(C_K2));
 	
+	//部分槽连接
+	//TODO
 
 }
 
+/*!
+* @brief 选择多张图片, 载入内存
+*        存入变量：vector<cv::Mat> checkerboardPatterns
+* @param NULL
+* @return NULL
+*/
 void CameraCalibration::on_loadImgBtn_clicked()
 {
-	qDebug() << "load images..." << endl;
+	//qDebug() << "load images..." << endl;
+	int imgNums = 0;
 	checkerboardPatterns.clear();
 	const QStringList imgPaths = QFileDialog::getOpenFileNames(this, tr("选取图片"), "", tr("图片格式 (*.png *.jpg);;All(*.*);;"));
 	if (!imgPaths.isEmpty())
@@ -24,28 +33,60 @@ void CameraCalibration::on_loadImgBtn_clicked()
 		cv::Mat img;
 		foreach(const QFileInfo &imgPath, imgPaths) {
 			QString imageFile = imgPath.absoluteFilePath();
-			img = cv::imread(imageFile.toStdString());
-			showImage(img, ui.imgViewer);
-			ui.imgViewer->repaint();  //强制更新
+			if (needCut) img = regular_img(cv::imread(imageFile.toStdString())); //是否裁剪
+			else img = cv::imread(imageFile.toStdString());
+			showImage(img, ui.CalibImgViewer);
+			ui.CalibImgViewer->repaint();  //强制更新
 			checkerboardPatterns.push_back(img);//保存图片
 		}
-		emit loadAllImages(checkerboardPatterns);//信号：载入所有图片
+
+		imgNums = checkerboardPatterns.size();
+		if (!imgNums) ui.imgLoadStatus->setText("尚未载入图片");
+		else
+		{	
+			ui.imgLoadStatus->setStyleSheet("color:black");
+			ui.imgLoadStatus->setText("载入图片数: " + QString::number(imgNums));
+		}
+		//emit loadAllImages(checkerboardPatterns);//信号：载入所有图片
 	}
 }
+
+/*!
+* @brief 计算畸变参数按钮槽
+* @param NULL
+* @return NULL
+*/
 void CameraCalibration::on_calparamsBtn_clicked()
 {
 	ui.calculateDisplay->clear();
 	calculateCameraParameters(checkerboardPatterns);
 }
 
+/*!
+* @brief 内角点数量输入槽
+* @param LineEdit输入
+* @return NULL
+*/
 void CameraCalibration::on_cornersOfColEdit_textChanged(const QString & num)
 {
 	this->cornerOfCol = num.toInt();
 }
+
+/*!
+* @brief 内角点数量输入槽
+* @param LineEdit输入
+* @return NULL
+*/
 void CameraCalibration::on_cornersOfRowEdit_textChanged(const QString & num)
 {
 	this->cornerOfRow = num.toInt();
 }
+
+/*!
+* @brief 是否沿x轴翻转
+* @param CheckBox状态
+* @return NULL
+*/
 void  CameraCalibration::on_xFlipCheck_stateChanged(int state)
 {
 	switch (state)
@@ -58,6 +99,12 @@ void  CameraCalibration::on_xFlipCheck_stateChanged(int state)
 		break;
 	}
 }
+
+/*!
+* @brief 是否沿y轴翻转
+* @param CheckBox状态
+* @return NULL
+*/
 void  CameraCalibration::on_yFlipCheck_stateChanged(int state)
 {
 	switch (state)
@@ -71,8 +118,98 @@ void  CameraCalibration::on_yFlipCheck_stateChanged(int state)
 	}
 }
 
-//QT显示图像
-//输入：imshowsrc 伪彩色图像
+/*!
+* @brief 是否四周裁剪图片选择槽
+* @param CheckBox状态
+* @return NULL
+*/
+void CameraCalibration::on_cutImgCheck_stateChanged(int state)
+{
+	switch (state)
+	{
+	case Qt::Checked:
+		this->needCut = true; break;
+	case Qt::Unchecked:
+		this->needCut = false; break;
+	default:
+		break;
+	}
+}
+
+/*!
+* @brief 矫正缓冲区下一张图片并显示
+* @param NULL
+* @return NULL
+*/
+void CameraCalibration::on_nextCalibImgBtn_clicked()
+{
+	ui.CalibImgResult->clear();
+	ui.CalibImgViewer->clear();
+	ui.calculateDisplay->clear();
+
+	if (checkerboardPatterns.size() < 1)
+		ui.calculateDisplay->setText("缓冲区无图片，请载入");
+	else
+	{
+		if (CurrentImgNum == checkerboardPatterns.size())CurrentImgNum = 0;
+		Mat imgShow = imageUndist(checkerboardPatterns[CurrentImgNum]);
+		showImage(checkerboardPatterns[CurrentImgNum++], ui.CalibImgViewer);
+		showImage(imgShow, ui.CalibImgResult);
+	}
+
+}
+
+/*!
+* @brief 矫正缓冲区上一张图片并显示
+* @param NULL
+* @return NULL
+*/
+void CameraCalibration::on_lastCalibImgBtn_clicked()
+{
+	ui.CalibImgResult->clear();
+	ui.CalibImgViewer->clear();
+	ui.calculateDisplay->clear();
+
+	if (checkerboardPatterns.size() < 1)
+		ui.calculateDisplay->setText("缓冲区无图片，请载入");
+	else
+	{
+		if (CurrentImgNum < 0)CurrentImgNum = checkerboardPatterns.size() - 1;
+		Mat imgShow = imageUndist(checkerboardPatterns[CurrentImgNum]);
+		showImage(checkerboardPatterns[CurrentImgNum--], ui.CalibImgViewer);
+		showImage(imgShow, ui.CalibImgResult);
+	}
+}
+
+/*!
+* @brief 应用矫正参数按钮槽(只做预览，不会保存）
+* @param NULL
+* @return NULL
+*/
+void CameraCalibration::on_calibParamSetBtn_clicked()
+{
+	if (!camereaMatrixTemp.at<double>(0, 0))  //缓存中没有记录
+	{
+		ui.currentParamShow->setText("缓冲区无数据\n请先计算矫正参数!\n");
+	}
+	else
+	{
+		this->C_FX = camereaMatrixTemp.at<double>(0, 0);
+		this->C_FY = camereaMatrixTemp.at<double>(1, 1);
+		this->C_CX = camereaMatrixTemp.at<double>(0, 2);
+		this->C_CY = camereaMatrixTemp.at<double>(1, 2);
+		this->C_K1 = distCoffeffsTemp.at<double>(0, 0);
+		this->C_K2 = distCoffeffsTemp.at<double>(0, 1);
+		ui.currentParamShow->setText("当前畸变参数:\n CX = " + QString::number(C_CX) + "\n CY = " + QString::number(C_CY) + "\n FX = " + QString::number(C_FX) + "\n FY = " + QString::number(C_FY) + "\n K1 = " + QString::number(C_K1) + "\n K2 = " + QString::number(C_K2));
+
+	}
+}
+
+/*!
+* @brief Qt显示图像
+* @param 伪彩色图像
+* @param 显示载体
+*/
 void CameraCalibration::showImage(cv::Mat imshowsrc, QLabel* label)
 {
 	cv::Mat imgShow = imshowsrc.clone();
@@ -88,12 +225,16 @@ void CameraCalibration::showImage(cv::Mat imshowsrc, QLabel* label)
 	label->setPixmap(QPixmap::fromImage(img)); //更新图像
 }
 
+//这个函数莫名其妙的多余...
 void CameraCalibration::textInsert(uchar num)
 {
 	ui.calculateDisplay->insertPlainText(QString::number(num));
 }
 
-//计算相机内外参
+/*!
+* @brief 计算相机内参
+* @param 原始图(经过翻转or裁剪）
+*/
 void CameraCalibration::calculateCameraParameters(vector<cv::Mat> srcImgs)
 {
 	int imgCount = 0; //图像计数
@@ -101,13 +242,16 @@ void CameraCalibration::calculateCameraParameters(vector<cv::Mat> srcImgs)
 	cv::Size board_size = cv::Size(cornerOfCol, cornerOfRow);//行列上的内角点数量
 	vector<cv::Point2f> per_img_point;//每张图检测的角点数量
 	vector<vector<cv::Point2f> > img_point;
+	cv::Mat img_gray;
+
 	ui.calculateDisplay->insertPlainText("寻找角点...");
 	for (int i = 0; i < srcImgs.size(); i++)
 	{
 		imgCount++;
-		if (needXFlip)flip(srcImgs[i], srcImgs[i], 0); //是否需要翻转图像
-		if (needYFlip)flip(srcImgs[i], srcImgs[i], 1);
+		if (needXFlip) flip(srcImgs[i], srcImgs[i], 0); //是否需要翻转图像
+		if (needYFlip) flip(srcImgs[i], srcImgs[i], 1);
 		cv::Mat imgShow = srcImgs[i].clone();
+		//cv::cvtColor(srcImgs[i], img_gray, CV_RGB2GRAY);//转换为灰度图来通过亚像素精确化寻找角点坐标
 		//获取图像尺寸
 		if (imgCount == 1)
 		{
@@ -121,15 +265,14 @@ void CameraCalibration::calculateCameraParameters(vector<cv::Mat> srcImgs)
 		}
 		else
 		{
-			cv::Mat img_gray;
-			cv::cvtColor(srcImgs[i], img_gray, CV_RGB2GRAY);//转换为灰度图来通过亚像素精确化寻找角点坐标
 			// Size(5, 5)角点窗口的尺寸
+			cv::cvtColor(srcImgs[i], img_gray, CV_RGB2GRAY);//转换为灰度图来通过亚像素精确化寻找角点坐标
 			cv::find4QuadCornerSubpix(img_gray, per_img_point, cv::Size(5, 5));
 			img_point.push_back(per_img_point);
 			//在图像显示角点位置
 			drawChessboardCorners(imgShow, board_size, per_img_point, true);
-			showImage(imgShow, ui.imgViewer);
-			ui.imgViewer->repaint();//强制更新
+			showImage(imgShow, ui.CalibImgViewer);
+			ui.CalibImgViewer->repaint();//强制更新
 
 		}
 	}
@@ -183,44 +326,92 @@ void CameraCalibration::calculateCameraParameters(vector<cv::Mat> srcImgs)
 
 	//开始标定
 	calibrateCamera(object_points, img_point, img_size, camereaMatrix, distCoffeffs, rotaMatrix, transMatrix, 0);
+	camereaMatrixTemp = camereaMatrix.clone();//更新临时变量
+	distCoffeffsTemp = distCoffeffs.clone();
 	ui.calculateDisplay->insertPlainText("\n计算完成。");
-	ui.calculateDisplay->insertPlainText("\n相机内参矩阵：\n");
-	//camereaMatrix.forEach<double>
-	//(
-	//	[this](double &pixel, const int * position) -> void
-	//	{
-	//		camInternalMatrix.append(QString::number(pixel));
-	//		camInternalMatrix.append(", ");
+	ui.calculateDisplay->insertPlainText("\n相机内参矩阵：");
 
-	//		ui.calculateDisplay->insertPlainText(QString::number(pixel));
-	//	}
-	//);
+	ui.calculateDisplay->insertPlainText("\nFx= ");
+	ui.calculateDisplay->insertPlainText(QString::number(camereaMatrix.at<double>(0, 0), 'f', 16));
+	ui.calculateDisplay->insertPlainText("\nFy= ");
+	ui.calculateDisplay->insertPlainText(QString::number(camereaMatrix.at<double>(1, 1), 'f', 16));
+	ui.calculateDisplay->insertPlainText("\nCx= ");
+	ui.calculateDisplay->insertPlainText(QString::number(camereaMatrix.at<double>(0, 2), 'f', 16));
+	ui.calculateDisplay->insertPlainText("\nCy= ");
+	ui.calculateDisplay->insertPlainText(QString::number(camereaMatrix.at<double>(1, 2), 'f', 16));
 
-	//std::cout << camereaMatrix<<endl; 
-	for (int i = 0; i < camereaMatrix.rows; i++)
+	ui.calculateDisplay->insertPlainText("\n畸变系数：");
+	ui.calculateDisplay->insertPlainText("\nk1= ");
+	ui.calculateDisplay->insertPlainText(QString::number(distCoffeffs.at<double>(0, 0), 'f', 16));
+	ui.calculateDisplay->insertPlainText("\nk2= ");
+	ui.calculateDisplay->insertPlainText(QString::number(distCoffeffs.at<double>(0, 1), 'f', 16));
+
+
+}
+
+/*!
+* @brief 畸变矫正
+* @param 未经矫正的原始图
+* @return 校正后的图片
+*/
+cv::Mat CameraCalibration::imageUndist(cv::Mat src)
+{
+	//畸变矫正
+	Mat img;
+
+	//内参矩阵
+	Mat cameraMatrix = Mat::eye(3, 3, CV_64F); 		//3*3单位矩阵
+	cameraMatrix.at<double>(0, 0) = C_FX;
+	cameraMatrix.at<double>(0, 1) = 0;
+	cameraMatrix.at<double>(0, 2) = C_CX;
+	cameraMatrix.at<double>(1, 1) = C_FY;
+	cameraMatrix.at<double>(1, 2) = C_CY;
+	cameraMatrix.at<double>(2, 2) = 1;
+
+	//畸变参数
+	Mat distCoeffs = Mat::zeros(5, 1, CV_64F); 		//5*1全0矩阵
+	distCoeffs.at<double>(0, 0) = C_K1;
+	distCoeffs.at<double>(1, 0) = C_K2;
+	distCoeffs.at<double>(2, 0) = 0;
+	distCoeffs.at<double>(3, 0) = 0;
+	distCoeffs.at<double>(4, 0) = 0;
+
+	Size imageSize = src.size();
+	Mat map1, map2;
+	//参数1：相机内参矩阵
+	//参数2：畸变矩阵
+	//参数3：可选输入，第一和第二相机坐标之间的旋转矩阵
+	//参数4：校正后的3X3相机矩阵
+	//参数5：无失真图像尺寸
+	//参数6：map1数据类型，CV_32FC1或CV_16SC2
+	//参数7、8：输出X/Y坐标重映射参数
+	initUndistortRectifyMap(cameraMatrix, distCoeffs, Mat(), cameraMatrix, imageSize, CV_32FC1, map1, map2); 	//计算畸变映射
+	//参数1：畸变原始图像
+	//参数2：输出图像
+	//参数3、4：X\Y坐标重映射
+	//参数5：图像的插值方式
+	//参数6：边界填充方式
+	remap(src, img, map1, map2, cv::INTER_NEAREST); 																	//畸变矫正
+	return img;
+}
+
+/*!
+* @brief 裁剪图片
+* @param 原始图
+* @return 裁剪后的图片
+*/
+cv::Mat CameraCalibration::regular_img(cv::Mat image)
+{
+	Mat source;
+	cvtColor(image, source, CV_RGBA2RGB);
+	Mat dst = Mat::zeros(240, 320, source.type());
+
+	for (int i = 0; i < 320; i++)
 	{
-		
-		for (int j = 0; j < camereaMatrix.cols; j++)
+		for (int j = 0; j < 240; j++)
 		{
-			ui.calculateDisplay->insertPlainText(QString::number(camereaMatrix.at<double>(i, j), 'f', 16));
-			ui.calculateDisplay->insertPlainText(", ");
-		}
-		ui.calculateDisplay->insertPlainText("\n");
-	}
-	ui.calculateDisplay->insertPlainText("\n畸变系数：\n");
-
-	for (int i = 0; i < distCoffeffs.rows; i++)
-	{
-		for (int j = 0; j < distCoffeffs.cols; j++)
-		{
-			ui.calculateDisplay->insertPlainText(QString::number(distCoffeffs.at<double>(i, j), 'f', 16));
-			ui.calculateDisplay->insertPlainText("\n");
+			dst.at<Vec3b>(j, i) = source.at<Vec3b>(j + 6, i + 4);
 		}
 	}
-
-
-
-	
-
-
+	return dst;
 }
--- CameraCalibration/CameraCalibration/CameraCalibration.h
+++ CameraCalibration/CameraCalibration/CameraCalibration.h
@@ -21,14 +21,26 @@ class CameraCalibration : public QWidget
 public:
 	CameraCalibration(QWidget *parent = Q_NULLPTR);
 	void showImage(cv::Mat imshowsrc, QLabel * label);//显示图像
-	void textInsert(uchar num);
+	double C_CX = 165.6507875355497,
+		C_CY = 120.9800873314841,
+		C_FX = 186.0929297122383,
+		C_FY = 186.5344394192148,
+		C_K1 = -0.2636080098584763,
+		C_K2 = 0.02495287341003726;
+
 private:
 	Ui::CameraCalibrationClass ui;
 	std::vector<cv::Mat> checkerboardPatterns;
-	int cornerOfRow = 6;
-	int cornerOfCol = 4;
-	bool needXFlip, needYFlip;
+	cv::Mat camereaMatrixTemp = cv::Mat(3, 3, CV_32FC1, cv::Scalar::all(0));//内参矩阵
+	cv::Mat distCoffeffsTemp = cv::Mat(1, 5, CV_32FC1, cv::Scalar::all(0)); //畸变系数
+	int cornerOfRow;
+	int cornerOfCol;
+	int CurrentImgNum = 0; //畸变预览图片位置
+	bool needXFlip = false, needYFlip = false, needCut = true;
+	void textInsert(uchar num);
 	void calculateCameraParameters(std::vector<cv::Mat> srcImgs);
+	cv::Mat imageUndist(cv::Mat src);
+	cv::Mat regular_img(cv::Mat image);
 	
 private slots:
 	void on_loadImgBtn_clicked();
@@ -37,7 +49,10 @@ private slots:
 	void on_cornersOfRowEdit_textChanged(const QString & num);
 	void on_xFlipCheck_stateChanged(int state);
 	void on_yFlipCheck_stateChanged(int state);
-
+	void on_cutImgCheck_stateChanged(int state);				//裁剪图片选择
+	void on_nextCalibImgBtn_clicked();							//畸变矫正预览
+	void on_lastCalibImgBtn_clicked();
+	void on_calibParamSetBtn_clicked();							//应用畸变系数
 signals:
 	void loadAllImages(std::vector<cv::Mat> imgVectors);
 };
